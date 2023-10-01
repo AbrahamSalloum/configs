c0f1509b0f6b177653b911ecf207283b6e2b1663
Abraham Salloum <abrahamsalloum@gmail.com>
2023-10-01 17:52:47 +1100

    Add files via upload

---
+++ imageconvert.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import os, sys
+
+path = os.getcwd()+'\\images\\'
+print(path)
+dirs = os.listdir( path )
+quality = 80
+resample= Image.Resampling.LANCZOS
+
+def resize():
+    
+    def save(new_height, new_width=None, aspect_ratio=None, appendString=""):
+        if aspect_ratio and not new_width:
+            new_width = new_height * aspect_ratio
+        if aspect_ratio and new_width:
+            new_width = new_width
+        if aspect_ratio is None and new_width: 
+            new_width = new_width
+        if aspect_ratio is None and new_width is None: 
+            return "Either Specify aspect_ratio or new_width, new_width takes precedence"
+
+        imResize = im.resize((int(new_width),int(new_height)), resample)
+        imResize.save(f + appendString+".jpg", 'JPEG',  quality=quality)
+        return 
+
+
+    for item in dirs:
+
+        if os.path.isfile(path+item):
+            
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            
+            aspect_ratio = im.width / im.height 
+            new_height = 1080
+            save(new_height, None, aspect_ratio, "_1080")
+            new_height = 500   
+            save(new_height, None, aspect_ratio, "_500")
+            new_height = 125    
+            save(new_height, None, aspect_ratio, "_thumb")
+
+resize()
